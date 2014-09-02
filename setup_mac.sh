761a3b18b5d0b968c7d086f0f5e91207a0801410
Naohisa Orita <naohisa-orita@gmo.jp>
2014-09-02 23:14:05 +0900

    setup for mac

---
+++ setup_mac.sh
@@ -0,0 +1,6 @@
+
+ln -s ~/github/mydotfiles/.screenrc ~/
+ln -s ~/github/mydotfiles/.tmux.conf ~/
+ln -s ~/github/mydotfiles/vimrc ~/.vimrc
+ln -s ~/github/mydotfiles/dot.zshrc ~/.zshrc
+ln -s ~/github/mydotfiles/dot.zprofile.mac ~/.zprofile
